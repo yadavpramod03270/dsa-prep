b912928c26b2e02d335fb27076a8a43f4696c540
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-08-12 11:04:56 +0530

    Time: 107 ms (8.82%), Space: 104.5 MB (6.38%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        for(int i =0; i<nums.size(); i++){
-            int ind=abs(nums[i]);// taking value from the array 
-            if(nums[ind]<0){ // checking if it is negetive, and if yes ....it means we have intialy store it and marked is as negetive
-                return ind;
-            }
-            nums[ind]=-nums[ind]; // if value not -ive , mark it 
+     unordered_map<int,int>mp;
+     for(auto x: nums){
+        mp[x]++;
+     }
+     for( const auto &[key,val]: mp){
+        if(val>=2){
+            return key;
         }
-        return -1;
+     }
+     return -1;
     }
 };
