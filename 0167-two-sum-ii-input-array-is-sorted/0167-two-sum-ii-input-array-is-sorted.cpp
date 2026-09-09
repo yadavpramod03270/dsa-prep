48e6874f57ede4e2d55b49d6f95e361c3bb6ece8
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-09-09 14:36:15 +0530

    Time: 0 ms (100%), Space: 19.4 MB (94.98%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-        unordered_map<int,int>mp;
-        for(int i=0; i<nums.size(); i++){
-            if(mp.contains(target-nums[i])){
-                v.push_back(mp[target - nums[i]] + 1); 
-                v.push_back(i+1);
-                break;
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;        
+        while (left < right) {
+            int current_sum = numbers[left] + numbers[right];
+            
+            if (current_sum == target) {
+                // Return 1-indexed positions
+                return {left + 1, right + 1};
+            } 
+            else if (current_sum < target) {
+                // Sum is too small, move the left pointer to increase the sum
+                left++;
+            } 
+            else {
+                // Sum is too large, move the right pointer to decrease the sum
+                right--;
             }
-            mp[nums[i]]=i;
         }
-        return v;
+        
+        return {}; // Fallback, though a solution is guaranteed to exist
     }
 };
