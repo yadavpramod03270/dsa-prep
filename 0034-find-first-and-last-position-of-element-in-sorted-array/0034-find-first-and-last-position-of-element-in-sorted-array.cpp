3abf1d7b300dd1ed2c4e3beb561d1d216575091c
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-09-17 10:54:41 +0530

    Time: 3 ms (0.79%), Space: 17.4 MB (88.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    
-        vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>v;
-        // here we have to use two binary search , one is for first , and one is for last
-        int start=0;
-        int end=nums.size()-1;
-        int first=-1;
-        int last=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                first=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-
-            }else{
-                start=mid+1;
-            }
+vector<int> searchRange(vector<int>& nums, int target) {
+    int first=-1;
+    int last=-1;
+    int i=0; 
+    int j=nums.size()-1;
+    while(i<=j){
+        int mid=i+(j-i)/2;
+        if(nums[mid]==target){
+            first=mid;
+            j=mid-1;
         }
-
-        start=0;
-         end=nums.size()-1;
-         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                last=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-
-            }else{
-                start=mid+1;
-            }
+        else if(nums[mid]>target){            
+            j=mid-1;
         }
-        v.push_back(first);
-        v.push_back(last);
-        return v;
+        else{
+            i=mid+1;
         }
-           
+    }
+    int start=0; 
+    int end=nums.size()-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            last=mid;
+            start=mid+1;
+        }
+        else if(nums[mid]>target){            
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return {first,last};
+
+}           
 };
