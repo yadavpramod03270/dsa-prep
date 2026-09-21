503eb69001933faf67950e4addacc7823701dccf
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-09-21 11:37:11 +0530

    Time: 1199 ms (7.69%), Space: 293.7 MB (6.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,28 +3,26 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
     int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> set;
-        vector<vector<int>> output;
+        set<vector<int>> st;
+        vector<vector<int>> res;
         for(int i=0; i<n-2; i++){
             int low = i+1, high = n-1;
-            while(low < high){
-                if(nums[i] + nums[low] + nums[high] < 0){
-                    low++;
-                }
-                else if(nums[i] + nums[low] + nums[high] > 0){
-                    high--;
-                }
-                else{
-                    set.insert({nums[i], nums[low], nums[high]});
-                    low++;
-                    high--;
-                }
-            }
+     while(low<high){
+        if(nums[i]+nums[low]+nums[high]<0){
+            low++;
         }
-        for(auto it : set){
-            output.push_back(it);
+        else  if(nums[i]+nums[low]+nums[high]>0){
+            high--;
         }
-        return output;
-    
+        else{
+            st.insert({nums[i],nums[low],nums[high]});
+            low++;
+            high--;
+        }
+     }}
+     for(auto& x: st){
+        res.push_back(x);
+     }
+     return res;
     }
 };
