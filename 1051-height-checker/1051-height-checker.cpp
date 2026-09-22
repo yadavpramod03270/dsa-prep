1a681bd2fb94520f45c88cd2cfc7d95ef7b09a22
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-09-22 11:51:19 +0530

    Time: 0 ms (100%), Space: 11.2 MB (82.56%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count=0;
+        vector<int> expected(heights.begin(), heights.end());
+        sort(expected.begin(), expected.end());
+        for(int i=0; i<heights.size(); i++){
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
